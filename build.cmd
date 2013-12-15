13cb566ae8fcd016a58fac55989bb4e01757f5b4
leekelleher <leekelleher@gmail.com>
2013-12-15 19:56:11 +0000

    Added the MSBuild tasks (assembilies)
    The 'fetch-msbuild-tasks.cmd' wasn't working as originall intended.

--- build.cmd
+++ build.cmd
@@ -1,6 +1,3 @@
-:: Fetch the MSBuild tasks (via NuGet)
-CALL .\tools\fetch-msbuild-tasks.cmd
-
 :: Build the package zip
 C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe package\package.proj
 
