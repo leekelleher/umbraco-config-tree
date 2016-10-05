f19648bfd67d45b34bf7637f66fd32802223bc03
TimGeyssens <tg@umbraco.com>
2016-10-05 17:11:49 +0200

    Try to get build up and running

--- src/Our.Umbraco.Tree.Config/Properties/VersionInfo.cs
+++ src/Our.Umbraco.Tree.Config/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("3.1.1")]
-[assembly: AssemblyFileVersion("3.1.2228.64167")]
+[assembly: AssemblyFileVersion("3.1.1307.46389")]
 [assembly: AssemblyInformationalVersion("3.1.1")]
 
 
